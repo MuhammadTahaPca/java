1014debe922565d144de551dc027e869894e618e
MuhammadTahaPca <muhammadtaha94@gmail.com>
2020-06-15 15:02:02 +0500

    Practise with new

---
+++ app.js
@@ -0,0 +1,105 @@
+// //  Chap 1
+// //  Task 1:
+// alert("Hello world")
+
+// // Task 2:
+// alert("Error! Please enter a valid password.")
+
+// // Task 3:
+// alert("Welcome to JS Land...\n Happy Coding!")
+
+// // Task 5
+// console.log("Hello... I can run JS through my web browser's console")
+
+// // Chap2
+// var title ="Muhammad Taha"
+// alert(title)
+
+// // task 4
+// var name ="Jhone Doe"
+// var age ="15 years"
+// var award="Certified Mobile Application Development"
+// alert(name)
+// alert(age)
+// alert(award)
+
+// // task 5
+// var x="Pizza \n Pizz \n Piz \n Pi \n P"
+// alert(x)
+
+// // task 6
+// var x ="My email address is "
+// var email ="muhammadtaha94@gmail.com"
+// alert(x + email)
+ 
+// // task7
+// var x ="I am trying to learn from the Book "
+// var y ="A smarter way to learn JavaScript"
+// alert(x + y)
+
+// // Chap 3
+// // task 1
+// var age ="I am 25 years old"
+// alert(age)
+
+// chap 4
+// task 3
+
+// document.write("<h1> Rules for naming JS variables</h1> <br> Variable name can only contain, numbers, $ and _ ")
+// document.write(" <br> Variable name must begin with a letter, $ or _")
+// document.write(" <br> Variable names are case sensative")
+// document.write(" <br> Variable name should not be JS keyword")
+
+//  Chap 9
+//  task 1
+
+// var name = prompt("Enter your city");
+// if (name == "Karachi"){
+//     alert("Welcome to city of lights")
+// }
+// else{
+//     alert("Wrong city")
+// }
+
+// task 2
+
+// var gender = prompt("Enter your gender")
+// if(gender == "male"){
+//     alert("Good Morning Sir.")
+// }
+// else{
+//     alert("Good Morning Ma'am")
+// }
+
+// task 3
+
+// var name =prompt("Enter Signal color")
+// if(name == "red"){
+// alert("Must stop")
+// }
+// else if(name == "yellow"){
+//     alert("Ready to move")
+// }
+// else if(name == "green"){
+//     alert("Move now")
+// }
+
+// task 4
+
+// var Number = prompt("Remaining fuel in car")
+// if(Number < 0.25){
+//     alert("Please refill the fuel in car")
+// }
+// else{
+//     alert("No")
+// }
+
+// task 5
+
+// var a = 4; if (++a === 5){ alert("given condition for variable a is true"); }
+// if("car" < "cat"){ alert("car is smaller than cat"); }
+// var b = 82; if (b++ === 83){ alert("given condition for variable b is true"); }
+// var c = 12; if (c++ === 13){ alert("condition 1 is true"); }
+// if (c === 13){ alert("condition 2 is true"); } if (++c < 14){ alert("condition 3 is true"); } if(c === 14){ alert("condition 4 is true"); }
+// var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){ alert("The cost equals"); }
+// if (true){ alert("True"); } if (false){ alert("False"); }
